6dbd165a5fd8d341a2df892b9f7a0377148087b8
hardlyPw <80461161+hardlyPw@users.noreply.github.com>
2021-04-17 13:57:16 +0900

    Add files via upload

---
+++ len.py
@@ -0,0 +1,27 @@
+a=int(input())
+b=int(input())
+c=len(str(a))
+d=len(str(b))
+k=1
+h=1
+e=1
+print(type(c))
+while True:
+    if(k==c):
+        print(k)
+        while True:
+            h=h+1
+            if(h==d):
+                print(h)
+                break
+
+        #여기서부터
+        #if (h%2==0):
+            for (
+                if (a[c/2-Z]==a[c/2+Z]):
+                    #~
+                Z=Z+1
+            
+
+    k=k+1
+
